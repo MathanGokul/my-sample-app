eab8f5af74e588d28559f8aa5a41012ad65dd46d
Mathana Gopl B <mathanagopal.gokul@gmail.com>
2019-10-02 16:31:15 +0530

    binding events

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
 })
 
 export class Test2Component {
+  empId = 151;
+  empName = 'Mathan';
+  submit = false;
+  SubmitString = 'data not submit';
+  DataInput = '';
+  getEmpName() {
+     return this.empName;
+  }
+  constructor() {
+    setTimeout(() => {
+      this.buttonEnableDisable(true);
+    }, 2000);
+  }
+  private submitData(event: any) {
+    console.log(event);
+    this.SubmitString = 'data submitted';
+    this.buttonEnableDisable(false);
+  }
+  private inputData(event) {
+     this.DataInput = event.target.value;
+  }
+  private buttonEnableDisable(button): boolean {
+     this.submit = button;
+  }
 }
